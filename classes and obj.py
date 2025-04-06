9e1bbb35d1c04658e011f644d9338ad72672b48e
barathkumar A <devbarath4602@gmail.com>
2025-04-06 20:30:09 +0530

    Add files via upload

---
+++ classes and obj.py
@@ -0,0 +1,16 @@
+class laptop():
+    def project(self,name):
+        print(name)
+    def display(self,name):
+        print("name is:",name)
+
+t1=laptop()
+t1.project("barath")
+t1.display("kumar")
+
+
+       
+
+
+
+
